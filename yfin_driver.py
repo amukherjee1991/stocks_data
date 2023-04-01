5853913dbcf56c2a8db5532d4cb600e8d4391d72
Arghya Mukherjee <39413425+amukherjee1991@users.noreply.github.com>
2023-03-31 19:24:41 -0500

    added code to download data for all ticker: download_exchange_data_multi

--- yfin_driver.py
+++ yfin_driver.py
@@ -1,5 +1,6 @@
 from yfin_data import YFStocks
 
+
 # Instantiate the YFStocks class with your Twelve Data API key
 api_key = "d3b7c1458amsh18c268d298148c9p1f55ccjsn9a161d57b562"
 yf_stocks = YFStocks(api_key)
@@ -8,5 +9,9 @@ yf_stocks = YFStocks(api_key)
 exchange = "NASDAQ"
 from twelve_data import RequestStocks
 
-yf_stocks.download_exchange_data(exchange).to_csv("Nasdaq_stocks.csv",index=False)
+# yf_stocks.download_exchange_data(exchange,30).to_csv("Nasdaq_stocks.csv",index=False)
+# print ("File Saved")
+
+yf_stocks.download_exchange_data_multi("NASDAQ").to_csv("NASDAQ_historical.csv",index=False)
+print ("File Saved")
 # nyse = make_request("NYSE")
